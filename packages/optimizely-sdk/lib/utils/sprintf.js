9b0db02bb9862a6abe75491e2cf88f0827769ff6
pauloptimizely <paul.serraino@optimizely.com>
2018-12-12 10:12:59 -0600

    fix sprintf hack

--- packages/optimizely-sdk/lib/utils/sprintf.js
+++ packages/optimizely-sdk/lib/utils/sprintf.js
@@ -1,12 +1,17 @@
 var sprintf2 = require('sprintf-js').sprintf;
 
 var sprintf = function(message, moduleName, data) {
-  var dataValues = Object.keys(data).map(function(k) { return data[k]; });
-  try {
-    return Object.assign({}, data, {
-      message: sprintf2.apply(sprintf2, [message, moduleName].concat(dataValues)),
-    });
-  } catch (error) {}
+  if (typeof data === 'object') {
+    var dataValues = Object.keys(data).map(function(k) { return data[k]; });
+    try {
+      return Object.assign({}, data, {
+        message: sprintf2.apply(sprintf2, [message, moduleName].concat(dataValues)),
+      });
+    } catch (error) {}
+  }
+  return {
+    message: sprintf2(message, moduleName),
+  };
 };
 
 module.exports = sprintf;
