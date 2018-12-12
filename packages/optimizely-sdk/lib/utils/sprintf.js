67a1dca472d727c30d70d1964b67f5a9f549ce21
pauloptimizely <paul.serraino@optimizely.com>
2018-12-12 09:49:07 -0600

    hackweek

--- packages/optimizely-sdk/lib/utils/sprintf.js
+++ packages/optimizely-sdk/lib/utils/sprintf.js
@@ -1,11 +1,13 @@
 const sprintf2 = require('sprintf-js').sprintf;
 
 const sprintf = (message, moduleName, data) => {
-  if (typeof data === 'string') return;
   const dataValues = Object.keys(data).map(k => data[k]);
-  return {
-    ...data,
-    message: sprintf2.apply(sprintf2, [message, moduleName, ...dataValues]),
+  try {
+    return {
+      ...data,
+      message: sprintf2.apply(sprintf2, [message, moduleName, ...dataValues]),
+    }
+  } catch (error) {
   }
 };
 
